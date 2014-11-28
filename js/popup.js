b902d9b230901b93a6a3e36f8a8b6f7f3de4ea85
fz <super9192010@gmail.com>
2014-11-28 22:18:56 +0800

    表格样式 及 显示判断

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,25 @@
 function showMessage(response) {
     if(response){
         var message = document.getElementById("message");
-        message.innerHTML = response;
+        // 如果JSON.parse(response);出现异常则直接显示response
+        try{
+            var result = JSON.parse(response);
+            var table = "<table>";
+            table += "<tr><th>ID</th><th>权限组名称</th></tr>";
+            for(var i=0; i<result.rows.length; i++){
+                var record = result.rows[i];
+                table += "<tr>";
+                table += "<td>" + record.ID + "</td>";
+                table += "<td>" + record.SELF_ASSIGNED_NAME + "</td>";
+                table += "</tr>";
+            }
+            table += "</table>";
+            message.innerHTML = table;
+        }catch(e){
+            alert(response);
+            message.innerHTML = "需要登录erp后使用";
+        }
+
     }else{
         window.close();
     }
