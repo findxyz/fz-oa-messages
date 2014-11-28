67c7f192b73afa27c6cc59e06feb6ba5a1a0229b
fz@zzwh <super9192010@gmail.com>
2014-11-28 16:38:43 +0800

    popup.js get oa messages from content.js by ajax

--- js/content.js
+++ js/content.js
@@ -1,10 +1,30 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request.action);
-        if (request.action == "content")
-            sendResponse({message: "message from content"});
+        if (request.action == "content"){
+            httpRequest('http://erp.cranewh.com:8001/system/userOwnAssignedManager/listUserOwnAssigned.do', sendResponse);
+            /*
+            the reason for return true
+
+            http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+
+            This function becomes invalid when the event listener returns,
+            unless you return true from the event listener to indicate you wish to send a response asynchronously
+            (this will keep the message channel open to the other end until sendResponse is called).
+
+            So you just need to add return true;
+            after the call to getUrls to indicate that you'll call the response function asynchronously.
+            */
+            return true;
+        }
     });
 
-setInterval(function () {
-    console.log("I'am content.js");
-}, 1000);
+function httpRequest(url, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
